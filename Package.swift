8f451ac914b6d50d5a3992b6a35abde94e4a2184
Liam Lindner <liam@mux.com>
2023-03-27 15:01:18 -0700

    Track upload statistics (#3)
    * Track upload statistics using Sentry
    
    * Remove Sentry from SDK and instead report to a Mux Endpoint which calls Sentry
    
    * Fix indent
    
    * Call the endpoint events instead of cors
    
    * POST Events to tray.io and include more metadata
    
    * Use mobile.muxanalytics.com to report upload events
    
    * Fix casing
    
    * Update Version string and use it
    
    * Better handling of duration for AVAsset
    
    * Remove sharedInstance of Reporter in favor of new

--- Package.swift
+++ Package.swift
@@ -16,9 +16,7 @@ let package = Package(
             targets: ["MuxUploadSDK"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
+    dependencies: [ ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
---
+++ Sources/MuxUploadSDK/Internal Utilities/Reporter.swift
@@ -0,0 +1,60 @@
+//
+//  Reporter.swift
+//  
+//
+//  Created by Liam Lindner on 3/16/23.
+//
+
+import Foundation
+
+class Reporter: NSObject {
+    var session: URLSession?
+    var pendingUploadEvent: UploadEvent?
+
+    override init() {
+        super.init()
+
+        let sessionConfig: URLSessionConfiguration = URLSessionConfiguration.default
+        session = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: nil)
+    }
+
+    func report(startTime: TimeInterval, endTime: TimeInterval, fileSize: UInt64, videoDuration: Double) -> Void {
+        self.pendingUploadEvent = UploadEvent(startTime: startTime, endTime: endTime, fileSize: fileSize, videoDuration: videoDuration)
+
+        let request = self.generateRequest(url: URL(string: "https://mobile.muxanalytics.com")!)
+
+        let dataTask = session?.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            self.pendingUploadEvent = nil
+        })
+        dataTask?.resume()
+    }
+
+    private func generateRequest(url: URL) -> URLRequest {
+        let request = NSMutableURLRequest(url: url,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        do {
+            let encoder = JSONEncoder()
+            encoder.keyEncodingStrategy = JSONEncoder.KeyEncodingStrategy.convertToSnakeCase
+            let jsonData = try encoder.encode(self.pendingUploadEvent)
+            request.httpBody = jsonData
+        } catch _ as NSError {}
+
+        return request as URLRequest
+    }
+}
+
+extension Reporter: URLSessionDelegate, URLSessionTaskDelegate {
+    public func urlSession(_ session: URLSession, task: URLSessionTask, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, completionHandler: @escaping (URLRequest?) -> Swift.Void) {
+        if(self.pendingUploadEvent != nil) {
+            if let redirectUrl = request.url {
+                let request = self.generateRequest(url: redirectUrl)
+                completionHandler(request)
+            }
+        }
+    }
+}
