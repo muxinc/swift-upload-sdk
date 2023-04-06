18f1521c74f143351135f7e2f8bf368d5572fd0c
Emily Dixon <edixon@mux.com>
2023-04-06 10:54:47 -0700

    Fix it

--- Sources/MuxUploadSDK/Internal Utilities/ChunkedFile.swift
+++ Sources/MuxUploadSDK/Internal Utilities/ChunkedFile.swift
@@ -72,6 +72,7 @@ class ChunkedFile {
     public func seekTo(byte: UInt64) throws {
         // Worst case: we start from the begining and there's a few very quick chunk successes
         try fileHandle?.seek(toOffset: byte)
+        NSLog("ChunkedFile: seekTo offset \(byte)")
         filePos = byte
     }
     
