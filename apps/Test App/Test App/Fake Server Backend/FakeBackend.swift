912896552085168de38c1fdfc9e41d2e90cefbf2
Emily Dixon <edixon@mux.com>
2023-05-09 15:35:52 -0700

    Do the upload backend

--- apps/Test App/Test App/Fake Server Backend/FakeBackend.swift
+++ apps/Test App/Test App/Fake Server Backend/FakeBackend.swift
@@ -8,16 +8,63 @@
 import Foundation
 
 /// This class "fakes" the server backend necessary to compvare an upload workflow.
-///  In your production use case, a backend server should take care of creating upload URLs
+/// In your production use case, a backend server should take care of creating upload URLs
 ///
-/// **In your real application, you should never build Mux server API ceredentials into your app**.
-/// It is done in this example for brevity and clarity only
+/// **You should never build Mux server API ceredentials into a real app**. We do it in this example for brevity only
 class FakeBackend {
     
+    func createDirectUpload() async throws -> URL {
+        let request = try {
+            var req = try URLRequest(url:fullURL(forEndpoint: "uploads"))
+            req.httpBody = try jsonEncoder.encode(NewAssetSettings())
+            return req
+        }()
+        
+        let (data, response) = try await urlSession.data(for: request)
+        if let httpResponse = response as? HTTPURLResponse,
+            (200...299).contains(httpResponse.statusCode) {
+            let responseData = try jsonDecoder.decode(CreateUploadResponseContainer.self, from: data).data
+            guard let uploadURL = URL(string:responseData.url) else {
+                throw CreateUploadError(message: "invalid upload url")
+            }
+            return uploadURL
+        } else {
+            throw CreateUploadError(message: "not an http url session")
+        }
+    }
     
-    var MUX_ACCESS_KEY_ID = "YOUR ACCESS KEY ID"
-    var MUX_ACCESS_KEY_SECRET = "YOUR ACCESS KEY SECRET"
+    /// Generates a full URL for a given endpoint in the Mux Video public API
+    private func fullURL(forEndpoint: String) throws -> URL {
+        guard let url = URL(string: "https://api.mux.com/video/\(forEndpoint)") else {
+            throw CreateUploadError(message: "bad endpoint")
+        }
+        return url
+    }
+    
+    let urlSession: URLSession
+    let jsonEncoder: JSONEncoder
+    let jsonDecoder: JSONDecoder
+    
+    let MUX_ACCESS_KEY_ID = "YOUR ACCESS KEY ID"
+    let MUX_ACCESS_KEY_SECRET = "YOUR ACCESS KEY SECRET"
+    
+    init(urlSession: URLSession) {
+        self.urlSession = urlSession
+        self.jsonEncoder = JSONEncoder()
+        self.jsonEncoder.keyEncodingStrategy = JSONEncoder.KeyEncodingStrategy.convertToSnakeCase
+        self.jsonDecoder = JSONDecoder()
+        self.jsonDecoder.keyDecodingStrategy = JSONDecoder.KeyDecodingStrategy.convertFromSnakeCase
+    }
+    
+    convenience init() {
+        self.init(urlSession: URLSession(configuration: URLSessionConfiguration.default))
+    }
 }
+
+struct CreateUploadError : Error {
+    let message: String
+}
+
 
 fileprivate struct CreateUploadPost: Codable {
     var newAssetSettings: NewAssetSettings = NewAssetSettings()
@@ -26,7 +73,7 @@ fileprivate struct CreateUploadPost: Codable {
 
 fileprivate struct NewAssetSettings: Codable {
     var playbackPolicy: [String] = ["public"]
-    var passthrough: String = "Extra video data. This can be anything"
+    var passthrough: String = "Extra video data. This can be any data and it's for your use"
     var mp4Support: String = "standard"
     var normalizeAudio: Bool = true
     var test: Bool = false
