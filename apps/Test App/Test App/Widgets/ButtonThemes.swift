49953d93aeffa8c793e2d2ea26af4bc21d879e08
Liam Lindner <liam@mux.com>
2023-05-10 18:18:49 -0700

    Minor indentation fixes

--- apps/Test App/Test App/Widgets/ButtonThemes.swift
+++ apps/Test App/Test App/Widgets/ButtonThemes.swift
@@ -12,12 +12,12 @@ import SwiftUI
 struct CtaButtonStyle : ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-                    .padding()
-                    .background(Rectangle().fill(.blue))
-                    .foregroundColor(.white)
-                    .clipShape(Capsule())
-                    .scaleEffect(configuration.isPressed ? 1.2 : 1)
-                    .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+            .padding()
+            .background(Rectangle().fill(.blue))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
 }
 
