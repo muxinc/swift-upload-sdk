a2b2a71b50d06be52481f0f63b2f71a2eba2353b
Emily Dixon <edixon@mux.com>
2023-05-11 17:03:16 -0700

    Ok now we're getting somewhere hypothetically

--- apps/Test App/Test App/Screens/CreateUploadScreen.swift
+++ apps/Test App/Test App/Screens/CreateUploadScreen.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct CreateUploadScreen: View {
     var body: some View {
@@ -20,6 +21,8 @@ struct CreateUploadScreen: View {
 }
 
 struct ScreenContent: View {
+    @EnvironmentObject var uploadScreenViewModel: UploadScreenViewModel
+    
     var body: some View {
         ZStack {
             WindowBackground
@@ -30,10 +33,36 @@ struct ScreenContent: View {
 }
 
 struct EmptyView: View {
+    @EnvironmentObject var uploadScreenViewModel: UploadScreenViewModel
+    @State var inPickFlow = false // True when picking photos or resolving the related permission prompt
+    
+    private var pickerConfig: PHPickerConfiguration = {
+        let mediaFilter = PHPickerFilter.any(of: [.videos])
+        var photoPickerConfig = PHPickerConfiguration(photoLibrary: .shared())
+        photoPickerConfig.filter = mediaFilter
+        photoPickerConfig.preferredAssetRepresentationMode = .current
+        photoPickerConfig.selectionLimit = 1
+        if #available(iOS 15.0, *) {
+            photoPickerConfig.selection = .default
+        }
+        return photoPickerConfig
+    }()
+    
     var body: some View {
         VStack {
-            BigUploadCTA() {
-                // TODO: Launch the ImagePicker
+            BigUploadCTA() { [self] in
+                switch uploadScreenViewModel.photosAuthStatus {
+                case .can_auth(_): do {
+                    inPickFlow = true
+                    uploadScreenViewModel.requestPhotosAccess()
+                }
+                case .authorized(_): do {
+                    inPickFlow = true
+                }
+                case .cant_auth(_): do {
+                    NSLog("!! This app  cannot ask for or gain Photos access permissions for some reason. We don't expect to see this on a real device unless 'NSPhotoLibraryAddUsageDescription' is gone from the app plist")
+                }
+                }
             }
             .padding(
                 EdgeInsets(
@@ -43,7 +72,47 @@ struct EmptyView: View {
                     trailing: 20
                 )
             )
+            .disabled(shouldDisableButton())
+            .sheet(
+                isPresented: Binding<Bool>(
+                    get: { self.shouldShowPhotoPicker() },
+                    set: { value, _ in inPickFlow = value && isAuthorizedForPhotos() }
+                ),
+                content: { ImagePicker(
+                    pickerConfiguration: self.pickerConfig,
+                    delegate: { (images: [PHPickerResult]) in
+                        // Only 0 or 1 images can be selected
+                        if let firstVideo = images.first {
+                            inPickFlow = false
+                            uploadScreenViewModel.startUpload(video: firstVideo)
+                        }
+                    }
+                )}
+            )
             Spacer()
+        }
+    }
+    
+    private func shouldShowPhotoPicker() -> Bool {
+        if isAuthorizedForPhotos() {
+            return inPickFlow
+        } else {
+            return false
+        }
+    }
+    
+    private func shouldDisableButton() -> Bool {
+        if isAuthorizedForPhotos() {
+            return false
+        } else {
+            return inPickFlow
+        }
+    }
+    
+    private func isAuthorizedForPhotos() -> Bool {
+        switch uploadScreenViewModel.photosAuthStatus {
+        case .authorized(_): return true
+        default: return false
         }
     }
 }
@@ -51,6 +120,7 @@ struct EmptyView: View {
 struct CreateUploadScreen_Previews: PreviewProvider {
     static var previews: some View {
         CreateUploadScreen()
+            .environmentObject(UploadScreenViewModel())
     }
 }
 
@@ -59,6 +129,6 @@ struct EmptyView_Previews: PreviewProvider {
         ZStack {
             WindowBackground.ignoresSafeArea()
             EmptyView()
-        }
+        }.environmentObject(UploadScreenViewModel())
     }
 }
