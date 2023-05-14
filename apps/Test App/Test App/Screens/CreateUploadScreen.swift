aec24524f37f87c68e19b6d3bd0c1cc10c859448
Emily Dixon <edixon@mux.com>
2023-05-14 16:50:10 -0700

    Do Processing View

--- apps/Test App/Test App/Screens/CreateUploadScreen.swift
+++ apps/Test App/Test App/Screens/CreateUploadScreen.swift
@@ -30,7 +30,7 @@ fileprivate struct ScreenContent: View {
             switch exportState {
             case .not_started: EmptyView()
             case .preparing: EmptyView() // TODO
-            case .failure(let error): EmptyView() // TODO
+            case .failure(let error): ErrorView(error: error)
             case .ready(let (image, url)): EmptyView() // TODO
             }
             
@@ -85,6 +85,32 @@ fileprivate struct ErrorView: View {
     }
 }
 
+fileprivate struct ProcessingView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 4.0)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 1.0))
+                    .foregroundColor(Gray30)
+                    .background(Gray30.clipShape(RoundedRectangle(cornerRadius: 4.0)))
+                ProgressView()
+                    .foregroundColor(Gray30)
+            }
+            .padding(
+                EdgeInsets(
+                    top: 64,
+                    leading: 20,
+                    bottom: 0,
+                    trailing: 20
+                )
+            )
+            .frame(height: 228)
+            
+            Spacer()
+        }
+    }
+}
+
 fileprivate struct EmptyView: View {
     var body: some View {
         VStack {
@@ -104,7 +130,7 @@ fileprivate struct EmptyView: View {
 
 struct ContentContainer_Previews: PreviewProvider {
     static var previews: some View {
-        let exportState = ExportState.not_started
+        let exportState = ExportState.failure(nil)
         ScreenContent(exportState: exportState)
             .environmentObject(UploadCreationViewModel())
     }
@@ -134,6 +160,17 @@ struct ErrorView_Previews: PreviewProvider {
         ZStack {
             WindowBackground.ignoresSafeArea()
             ErrorView()
+        }
+        .environmentObject(UploadScreenViewModel())
+        .environmentObject(UploadCreationViewModel())
+    }
+}
+
+struct ProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            WindowBackground.ignoresSafeArea()
+            ProcessingView()
         }
         .environmentObject(UploadScreenViewModel())
         .environmentObject(UploadCreationViewModel())
