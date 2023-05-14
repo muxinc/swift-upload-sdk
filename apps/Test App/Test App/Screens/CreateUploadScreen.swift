e9b9878a25c43ed6683522d584ed77788777f507
Emily Dixon <edixon@mux.com>
2023-05-14 12:28:11 -0700

    Upload CTA can be smarter than this

--- apps/Test App/Test App/Screens/CreateUploadScreen.swift
+++ apps/Test App/Test App/Screens/CreateUploadScreen.swift
@@ -31,37 +31,9 @@ struct ScreenContent: View {
 }
 
 struct EmptyView: View {
-    @EnvironmentObject var uploadScreenViewModel: UploadScreenViewModel
-    @State var inPickFlow = true // True when picking photos or resolving the related permission prompt, or when first launching the screen
-    
-    private var pickerConfig: PHPickerConfiguration = {
-        let mediaFilter = PHPickerFilter.any(of: [.videos])
-        var photoPickerConfig = PHPickerConfiguration(photoLibrary: .shared())
-        photoPickerConfig.filter = mediaFilter
-        photoPickerConfig.preferredAssetRepresentationMode = .current
-        photoPickerConfig.selectionLimit = 1
-        if #available(iOS 15.0, *) {
-            photoPickerConfig.selection = .default
-        }
-        return photoPickerConfig
-    }()
-    
     var body: some View {
         VStack {
-            BigUploadCTA() { [self] in
-                switch uploadScreenViewModel.photosAuthStatus {
-                case .can_auth(_): do {
-                    inPickFlow = true
-                    uploadScreenViewModel.requestPhotosAccess()
-                }
-                case .authorized(_): do {
-                    inPickFlow = true
-                }
-                case .cant_auth(_): do {
-                    NSLog("!! This app  cannot ask for or gain Photos access permissions for some reason. We don't expect to see this on a real device unless 'NSPhotoLibraryAddUsageDescription' is gone from the app plist")
-                }
-                }
-            }
+            BigUploadCTA()
             .padding(
                 EdgeInsets(
                     top: 64,
@@ -70,47 +42,7 @@ struct EmptyView: View {
                     trailing: 20
                 )
             )
-            .disabled(shouldDisableButton())
-            .sheet(
-                isPresented: Binding<Bool>(
-                    get: { self.shouldShowPhotoPicker() },
-                    set: { value, _ in inPickFlow = value && isAuthorizedForPhotos() }
-                ),
-                content: { ImagePicker(
-                    pickerConfiguration: self.pickerConfig,
-                    delegate: { (images: [PHPickerResult]) in
-                        // Only 0 or 1 images can be selected
-                        if let firstVideo = images.first {
-                            inPickFlow = false
-                            uploadScreenViewModel.startUpload(video: firstVideo)
-                        }
-                    }
-                )}
-            )
             Spacer()
-        }
-    }
-    
-    private func shouldShowPhotoPicker() -> Bool {
-        if isAuthorizedForPhotos() {
-            return inPickFlow
-        } else {
-            return false
-        }
-    }
-    
-    private func shouldDisableButton() -> Bool {
-        if isAuthorizedForPhotos() {
-            return false
-        } else {
-            return inPickFlow
-        }
-    }
-    
-    private func isAuthorizedForPhotos() -> Bool {
-        switch uploadScreenViewModel.photosAuthStatus {
-        case .authorized(_): return true
-        default: return false
         }
     }
 }
--- apps/Test App/Test App/Widgets/BigUploadCTA.swift
+++ apps/Test App/Test App/Widgets/BigUploadCTA.swift
@@ -6,11 +6,38 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct BigUploadCTA: View {
+    @EnvironmentObject var uploadScreenViewModel: UploadScreenViewModel
+    @State var inPickFlow = false // True when picking photos or resolving the related permission prompt, or when first launching the screen
+    
+    private var pickerConfig: PHPickerConfiguration = {
+        let mediaFilter = PHPickerFilter.any(of: [.videos])
+        var photoPickerConfig = PHPickerConfiguration(photoLibrary: .shared())
+        photoPickerConfig.filter = mediaFilter
+        photoPickerConfig.preferredAssetRepresentationMode = .current
+        photoPickerConfig.selectionLimit = 1
+        if #available(iOS 15.0, *) {
+            photoPickerConfig.selection = .default
+        }
+        return photoPickerConfig
+    }()
+    
     var body: some View {
-        Button {
-            action()
+        Button { [self] in
+            switch uploadScreenViewModel.photosAuthStatus {
+            case .can_auth(_): do {
+                inPickFlow = true
+                uploadScreenViewModel.requestPhotosAccess()
+            }
+            case .authorized(_): do {
+                inPickFlow = true
+            }
+            case .cant_auth(_): do {
+                NSLog("!! This app  cannot ask for or gain Photos access permissions for some reason. We don't expect to see this on a real device unless 'NSPhotoLibraryAddUsageDescription' is gone from the app plist")
+            }
+            }
         } label : {
             ZStack {
                 RoundedRectangle(cornerRadius: 4.0)
@@ -27,12 +54,51 @@ struct BigUploadCTA: View {
             }
             .frame(height: 228)
         }
+        .contentShape(Rectangle())
+        .disabled(shouldDisableButton())
+        .sheet(
+            isPresented: Binding<Bool>(
+                get: { self.shouldShowPhotoPicker() },
+                set: { value, _ in inPickFlow = value && isAuthorizedForPhotos() }
+            ),
+            content: { ImagePicker(
+                pickerConfiguration: self.pickerConfig,
+                delegate: { (images: [PHPickerResult]) in
+                    // Only 0 or 1 images can be selected
+                    if let firstVideo = images.first {
+                        inPickFlow = false
+                        uploadScreenViewModel.startUpload(video: firstVideo)
+                    }
+                }
+            )}
+        )
     }
     
-    let action: () -> Void
+    private func shouldShowPhotoPicker() -> Bool {
+        if isAuthorizedForPhotos() {
+            return inPickFlow
+        } else {
+            return false
+        }
+    }
     
-    init(action: @escaping () -> Void = {}) {
-        self.action = action
+    private func shouldDisableButton() -> Bool {
+        if isAuthorizedForPhotos() {
+            return false
+        } else {
+            return inPickFlow
+        }
+    }
+    
+    private func isAuthorizedForPhotos() -> Bool {
+        switch uploadScreenViewModel.photosAuthStatus {
+        case .authorized(_): return true
+        default: return false
+        }
+    }
+    
+    
+    init(_ alsoAction: @escaping () -> Void = {}) {
     }
 }
 
@@ -42,6 +108,6 @@ struct BigUploadCTA_Preview: PreviewProvider {
             WindowBackground.ignoresSafeArea()
             BigUploadCTA()
                 .padding(EdgeInsets(top: 64, leading: 20, bottom: 0, trailing: 20))
-        }
+        }.environmentObject(UploadScreenViewModel())
     }
 }
