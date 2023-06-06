cadd935474533e0b6cf5c2e969c671fdc3d95b00
Emily Dixon <edixon@mux.com>
2023-06-06 15:22:35 -0700

    Add a bunch of stuff, including a command that builds docs

--- Package.swift
+++ Package.swift
@@ -17,7 +17,9 @@ let package = Package(
             targets: ["MuxUploadSDK"]
         ),
     ],
-    dependencies: [ ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.2.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
