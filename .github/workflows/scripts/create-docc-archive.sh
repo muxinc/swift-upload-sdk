bd875ab2fe46685f7785f4db6df3a490f60a2aca
Emily Dixon <edixon@mux.com>
2023-06-07 11:53:36 -0700

    Configure the name

--- .github/workflows/scripts/create-docc-archive.sh
+++ .github/workflows/scripts/create-docc-archive.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 # REQUIRED ENV VARS
 # This script is intended to be run by a GitHub Action that supplies these env vars:
-#   PROJECT_NAME: The name of the xcode project (without the extension)
-#   SCHEME: The scheme to build and generate docs for
-#   BUILD_CONFIGURATION: The build configuration to document
-#   XCCONFIG_FILENAME: The name of the xcodebuild config file
+#   target_scheme: The scheme to build and generate docs for
+#   devdocs_bucket: The destination bucket/directory name for this project
 
 readonly XCODE=$(xcodebuild -version | grep Xcode | cut -d " " -f2)
 
-#readonly TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 readonly TOP_DIR=$(pwd)
 readonly BUILD_DIR="${TOP_DIR}/.build"
 readonly DOCUMENTATION_DIR="${BUILD_DIR}/docs"
 readonly XCCONFIG_DIR="${TOP_DIR}/FrameworkXCConfigs/"
 
-# if [ -z $target_project ] 
-# then
-#     echo -e "\033[1;31m ERROR: Target project must be specified \033[0m"
-#     exit 1
-# else
-#   readonly PROJECT_NAME=$target_project
-# fi
+if [ -z $devdocs_bucket ]
+then
+    echo -e "\033[1;31m ERROR: Target devdocs bucket must be specified \033[0m"
+    exit 1
+else 
+  readonly DEVDOCS_BUCKET=$devdocs_bucket
+fi
 if [ -z $target_scheme ]
 then
     echo -e "\033[1;31m ERROR: Target scheme must be specified \033[0m"
@@ -28,20 +25,7 @@ then
 else 
   readonly SCHEME=$target_scheme 
 fi
-# if [ -z $target_build_configration ]
-# then
-#   readonly BUILD_CONFIGURATION="Release"
-# else 
-#   readonly BUILD_CONFIGURATION=$target_build_configration
-# fi
-# if [ -z $xcconfig_filename ]
-# then
-#   readonly XCCONFIG_FILENAME="Release-Production"
-# else
-#   readonly XCCONFIG_FILENAME=$target_xcconfig_filename
-# fi
 
-#readonly PROJECT="${TOP_DIR}/${PROJECT_NAME}.xcodeproj"
 readonly DOCC_ARCHIVE_NAME="${SCHEME}.doccarchive"
 readonly DOCC_ARCHIVE_PATH="${BUILD_DIR}/${DOCC_ARCHIVE_NAME}"
 
@@ -60,16 +44,6 @@ echo "▸ Using Xcode Version: ${XCODE}"
 echo "▸ Building Documentation Catalog for ${SCHEME}"
 
 mkdir -p $DOCUMENTATION_DIR
-
-#expanded_xcconfig_path="${XCCONFIG_DIR}/${XCCONFIG_FILENAME}.xcconfig"
-
-                    #-xcconfig $expanded_xcconfig_path \
-# xcodebuild docbuild -project $PROJECT \
-#                     -scheme $SCHEME \
-#                     -configuration $BUILD_CONFIGURATION \
-#                     -destination 'generic/platform=iOS' \
-#                     -sdk iphoneos \
-#                     -derivedDataPath "${DOCUMENTATION_DIR}" | xcbeautify \
 
 echo "▸ Building documentation archive at: ${DOCUMENTATION_DIR}"
 
@@ -98,7 +72,7 @@ fi
 # Name of S3 devdocs bucket top-level directory where SDK documentation
 # is deployed to
 # TODO: This needs to be configurable from outside
-static_documentation_root_directory_name="upload-ios"
+static_documentation_root_directory_name="${DEVDOCS_BUCKET}"
 
 # This matches the semantic version of the SDK binary
 # TODO: Set based on XCConfig
