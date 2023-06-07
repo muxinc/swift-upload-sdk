58bada29babfbbb0bca241af878c0c5fecf8846d
Emily Dixon <edixon@mux.com>
2023-06-07 14:27:10 -0700

    Use the updated script

--- .github/workflows/scripts/create-docc-archive.sh
+++ .github/workflows/scripts/create-docc-archive.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
-# REQUIRED ENV VARS
-# This script is intended to be run by a GitHub Action that supplies these env vars:
-#   target_scheme: The scheme to build and generate docs for
-#   devdocs_bucket: The destination bucket/directory name for this project
-#   sdk_semantic_version: The semantic version of the SDK, or 'staging'
 
 readonly XCODE=$(xcodebuild -version | grep Xcode | cut -d " " -f2)
 
-readonly TOP_DIR=$(pwd)
+#readonly TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+readonly TOP_DIR=`pwd`
+readonly WORKFLOWS_DIR=".github/workflows"
 readonly BUILD_DIR="${TOP_DIR}/.build"
 readonly DOCUMENTATION_DIR="${BUILD_DIR}/docs"
-readonly XCCONFIG_DIR="${TOP_DIR}/FrameworkXCConfigs/"
 
-if [ -z $devdocs_bucket ]
-then
-    echo -e "\033[1;31m ERROR: Target devdocs bucket must be specified \033[0m"
-    exit 1
-else 
-  readonly DEVDOCS_BUCKET=$devdocs_bucket
-fi
-if [ -z $target_scheme ]
-then
-    echo -e "\033[1;31m ERROR: Target scheme must be specified \033[0m"
-    exit 1
-else 
-  readonly SCHEME=$target_scheme 
-fi
-
+readonly SCHEME=${target_scheme}
 readonly DOCC_ARCHIVE_NAME="${SCHEME}.doccarchive"
 readonly DOCC_ARCHIVE_PATH="${BUILD_DIR}/${DOCC_ARCHIVE_NAME}"
+
+# Subdirectory of the devdocs S3 bucket where SDK static docs are stored
+# in a directory corresponding to the SDKs version. Ex: "${DOCS_ROOT_DIR}/1.0.0"
+readonly DOCS_ROOT_DIR=${devdocs_bucket}
 
 if ! command -v xcbeautify &> /dev/null
 then
@@ -46,12 +32,11 @@ echo "▸ Building Documentation Catalog for ${SCHEME}"
 
 mkdir -p $DOCUMENTATION_DIR
 
-echo "▸ Building documentation archive at: ${DOCUMENTATION_DIR}"
-
-xcodebuild docbuild -scheme ${SCHEME} \
+xcodebuild docbuild -scheme $SCHEME \
                     -destination 'generic/platform=iOS' \
+                    -sdk iphoneos \
                     -derivedDataPath "${DOCUMENTATION_DIR}" \
-                    | xcbeautify
+                    | xcbeautify 
 
 cd ${BUILD_DIR}
 
@@ -62,7 +47,7 @@ docc_built_archive_path=$(find docs -type d -name "${DOCC_ARCHIVE_NAME}")
 
 if [ -z "${docc_built_archive_path}" ]
 then
-    echo -e "\033[1;31m ERROR: Failed to locate Documentation Archive \033[0m"
+echo -e "\033[1;31m ERROR: Failed to locate Documentation Archive \033[0m"
     exit 1
 else
     echo "▸ Located documentation archive at ${docc_built_archive_path}"
@@ -70,26 +55,25 @@ else
     zip -qry "${DOCC_ARCHIVE_NAME}.zip" "${DOCC_ARCHIVE_NAME}"
 fi
 
-# Name of S3 devdocs bucket top-level directory where SDK documentation
-# is deployed to
-# TODO: This needs to be configurable from outside
-static_documentation_root_directory_name="${DEVDOCS_BUCKET}"
+#expanded_base_xcconfig_path="${XCCONFIG_DIR}/${BASE_XCCONFIG_FILENAME}.xcconfig"
 
-# This matches the semantic version of the SDK binary
-# TODO: Set based on XCConfig
-static_documentation_versioned_subdirectory_name="staging"
+# TODO: Obtained from the environment now
+# Locates the SDK marketing version and extracts it to a variable
+#
+# Uses the substitute (s) command to lines containing 'MARKETING_VERSION = ' 
+# substring then captures everything on that line after the subscript (\(.*\)) 
+# then prints the first such match
+#sdk_semantic_version="$(sed -n 's/MARKETING_VERSION = \(.*\)/\1/p' $expanded_base_xcconfig_path)"
 
 # When deploying a new version the contents of the latest subdirectory 
 # gets replaced with the generated documentation from the new version 
-static_documentation_latest_subdirectory_name="latest"
+latest_subdirectory_name="latest"
 
-static_documentation_versioned_path="${static_documentation_root_directory_name}/${static_documentation_versioned_subdirectory_name}"
+static_documentation_versioned_path="${DOCS_ROOT_DIR}/${sdk_semantic_version}"
 
 source_archive_path=${DOCC_ARCHIVE_PATH}
-output_path=$static_documentation_versioned_subdirectory_name
-output_path="./docc-output-processed"
+output_path=$sdk_semantic_version
 hosting_base_path=$static_documentation_versioned_path
-
 
 echo "▸ Processing documentation archive with source archive path: ${source_archive_path} hosting base path: ${hosting_base_path} output path: ${output_path}"
 
@@ -99,15 +83,21 @@ $(xcrun --find docc) process-archive transform-for-static-hosting "${source_arch
     --output-path "${output_path}" \
     --hosting-base-path "${hosting_base_path}"
 
-# Add a redirect to the actual doc page
-sed -e "s/__VERSION__/${sdk_semantic_version}/g" -e "s/__SLUG__/${DEVDOCS_BUCKET}/g" "../.github/workflows/scripts/index.html.template" > ${output_path}/index.html
+# Replace index.html with a redirect to documentation/muxspaces/
+sed -e "s/__VERSION__/${sdk_semantic_version}/" "../${WORKFLOWS_DIR}/scripts/index.html.template" > ${output_path}/index.html
 
-mkdir -p $static_documentation_root_directory_name
+mkdir -p $DOCS_ROOT_DIR
 
-mv $output_path $static_documentation_root_directory_name
+mkdir -p $DOCS_ROOT_DIR/$latest_subdirectory_name
 
-# TODO: Is this part required?
-zip -qry "${static_documentation_root_directory_name}.zip" "${static_documentation_root_directory_name}"
+# Replace index.html with a redirect to documentation/muxspaces/
+sed -e "s/__VERSION__/${latest_subdirectory_name}/" "../${WORKFLOWS_DIR}/scripts/index.html.template" > $DOCS_ROOT_DIR/${latest_subdirectory_name}/index.html
+
+cp -r ./$output_path/. ./$DOCS_ROOT_DIR/$latest_subdirectory_name
+
+mv $output_path $DOCS_ROOT_DIR
+
+zip -qry "${DOCS_ROOT_DIR}.zip" "${DOCS_ROOT_DIR}"
 
 cd ..
 
