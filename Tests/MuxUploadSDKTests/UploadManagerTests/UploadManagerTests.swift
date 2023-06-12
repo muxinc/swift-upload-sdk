16d45cdb991a75e0939ec09d0522d2128a4d875b
AJ Lauer Barinov <abarinov@mux.com>
2023-06-12 14:20:27 -0700

    Use the public init

--- Tests/MuxUploadSDKTests/UploadManagerTests/UploadManagerTests.swift
+++ Tests/MuxUploadSDKTests/UploadManagerTests/UploadManagerTests.swift
@@ -21,23 +21,7 @@ class UploadManagerTests: XCTestCase {
             URL(string: "file://path/to/dummy/file/")
         )
 
-        let chunkFileUploader = ChunkedFileUploader(
-            uploadInfo: UploadInfo(
-                id: UUID().uuidString,
-                uploadURL: uploadURL,
-                videoFile: videoInputURL,
-                chunkSize: 8,
-                retriesPerChunk: 2,
-                optOutOfEventTracking: true
-            )
-        )
-
         let upload = MuxUpload(
-            wrapping: chunkFileUploader,
-            uploadManager: uploadManager
-        )
-
-        let duplicateChunkFileUploader = ChunkedFileUploader(
             uploadInfo: UploadInfo(
                 id: UUID().uuidString,
                 uploadURL: uploadURL,
@@ -45,16 +29,24 @@ class UploadManagerTests: XCTestCase {
                 chunkSize: 8,
                 retriesPerChunk: 2,
                 optOutOfEventTracking: true
-            )
+            ),
+            uploadManager: uploadManager
         )
 
         let duplicateUpload = MuxUpload(
-            wrapping: duplicateChunkFileUploader,
+            uploadInfo: UploadInfo(
+                id: UUID().uuidString,
+                uploadURL: uploadURL,
+                videoFile: videoInputURL,
+                chunkSize: 8,
+                retriesPerChunk: 2,
+                optOutOfEventTracking: true
+            ),
             uploadManager: uploadManager
         )
 
-        upload.start(forceRestart: true)
-        duplicateUpload.start(forceRestart: true)
+        upload.start(forceRestart: false)
+        duplicateUpload.start(forceRestart: false)
 
         XCTAssertEqual(
             uploadManager.allManagedUploads().count,
