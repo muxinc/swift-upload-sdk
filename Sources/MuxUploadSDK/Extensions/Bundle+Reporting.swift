da2f52fd22636f9e843edb293060f92e3f8bce11
AJ Lauer Barinov <abarinov@mux.com>
2023-07-06 16:14:40 -0700

    Move Bundle convenience extension into its own file

---
+++ Sources/MuxUploadSDK/Extensions/Bundle+Reporting.swift
@@ -0,0 +1,15 @@
+//
+//  Bundle+Reporting.swift
+//  
+
+import Foundation
+
+extension Bundle {
+    var appName: String? {
+        return object(forInfoDictionaryKey: "CFBundleName") as? String
+    }
+
+    var appVersion: String? {
+        return object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+    }
+}
