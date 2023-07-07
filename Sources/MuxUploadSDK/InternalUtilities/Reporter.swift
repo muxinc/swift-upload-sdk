879df8636aa9761b6be636056cf8b76b6bbc5dbb
AJ Lauer Barinov <102617203+andrewjl-mux@users.noreply.github.com>
2023-07-07 12:27:07 -0700

    URL request constructor extension (#55)
    * Move request construction to extension
    
    * Update Sources/MuxUploadSDK/Extensions/File.swift
    
    Co-authored-by: Emily Dixon <edixon@mux.com>
    
    * Update file name
    
    ---------
    
    Co-authored-by: Emily Dixon <edixon@mux.com>

--- Sources/MuxUploadSDK/InternalUtilities/Reporter.swift
+++ Sources/MuxUploadSDK/InternalUtilities/Reporter.swift
@@ -63,32 +63,36 @@ class Reporter: NSObject {
             regionCode: regionCode
         )
 
-        let request = self.generateRequest(url: URL(string: "https://mobile.muxanalytics.com")!)
-
-        let dataTask = session?.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-            self.pendingUploadEvent = nil
-        })
-        dataTask?.resume()
+        // FIXME: If this fails, an event without a payload
+        // is sent which probably isn't what we want
+        do {
+            let httpBody = try serializePendingEvent()
+            let request = self.generateRequest(
+                url: URL(string: "https://mobile.muxanalytics.com")!,
+                httpBody: httpBody
+            )
+            let dataTask = session?.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                self.pendingUploadEvent = nil
+            })
+            dataTask?.resume()
+        } catch _ as NSError {}
     }
 
     func serializePendingEvent() throws -> Data {
         return try jsonEncoder.encode(pendingUploadEvent)
     }
 
-    private func generateRequest(url: URL) -> URLRequest {
+    private func generateRequest(
+        url: URL,
+        httpBody: Data
+    ) -> URLRequest {
         let request = NSMutableURLRequest(url: url,
                                           cachePolicy: .useProtocolCachePolicy,
                                           timeoutInterval: 10.0)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Accept")
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        // FIXME: If this fails, an event without a payload
-        // is sent which probably isn't what we want
-        do {
-            let jsonData = try serializePendingEvent()
-            request.httpBody = jsonData
-        } catch _ as NSError {}
+        request.httpBody = httpBody
 
         return request as URLRequest
     }
@@ -99,8 +103,11 @@ class Reporter: NSObject {
 extension Reporter: URLSessionDelegate, URLSessionTaskDelegate {
     func urlSession(_ session: URLSession, task: URLSessionTask, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, completionHandler: @escaping (URLRequest?) -> Swift.Void) {
         if(self.pendingUploadEvent != nil) {
-            if let redirectUrl = request.url {
-                let request = self.generateRequest(url: redirectUrl)
+            if let redirectUrl = request.url, let httpBody = try? serializePendingEvent() {
+                let request = self.generateRequest(
+                    url: redirectUrl,
+                    httpBody: httpBody
+                )
                 completionHandler(request)
             }
         }
