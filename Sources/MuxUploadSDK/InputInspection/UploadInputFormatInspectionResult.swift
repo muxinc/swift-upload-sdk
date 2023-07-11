716afcbd61c818249ba4ed2430b2a562a65fa1f1
AJ Lauer Barinov <abarinov@mux.com>
2023-07-11 12:40:14 -0700

    Include non standard input reasons

--- Sources/MuxUploadSDK/InputInspection/UploadInputFormatInspectionResult.swift
+++ Sources/MuxUploadSDK/InputInspection/UploadInputFormatInspectionResult.swift
@@ -56,3 +56,32 @@ enum UploadInputFormatInspectionResult {
     }
 
 }
+
+extension UploadInputFormatInspectionResult.NonstandardInputReason: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .audioCodec:
+            return "audio_codec"
+        case .audioEditList:
+            return "audio_edit_list"
+        case .pixelAspectRatio:
+            return "pixel_aspect_ratio"
+        case .videoBitrate:
+            return "video_bitrate"
+        case .videoCodec:
+            return "video_codec"
+        case .videoEditList:
+            return "video_edit_list"
+        case .videoFrameRate:
+            return "video_frame_rate"
+        case .videoGOPSize:
+            return "video_gop_size"
+        case .videoResolution:
+            return "video_resolution"
+        case .unexpectedMediaFileParameters:
+            return "unexpected_media_file_parameters"
+        case .unsupportedPixelFormat:
+            return "unsupported_pixel_format"
+        }
+    }
+}
