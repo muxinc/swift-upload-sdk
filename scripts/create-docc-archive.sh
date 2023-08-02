2e6d2b8e987ed9e9d4f4ddf67011580dab4e3b6e
AJ Lauer Barinov <abarinov@mux.com>
2023-08-02 12:39:47 -0700

    Add post-process script
    Remove duplicate docc archiving scripts

--- scripts/create-docc-archive.sh
+++ scripts/create-docc-archive.sh
@@ -2,26 +2,18 @@
 
 readonly XCODE=$(xcodebuild -version | grep Xcode | cut -d " " -f2)
 
-readonly TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+#readonly TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+readonly TOP_DIR=`pwd`
 readonly BUILD_DIR="${TOP_DIR}/.build"
 readonly DOCUMENTATION_DIR="${BUILD_DIR}/docs"
-readonly XCCONFIG_DIR="${TOP_DIR}/FrameworkXCConfigs/"
 
-readonly PROJECT_NAME="SwiftUploadSDKExample"
-readonly PROJECT_PATH="${TOP_DIR}/Example/SwiftUploadSDKExample/${PROJECT_NAME}.xcodeproj"
-readonly SCHEME="MuxUploadSDK"
-readonly BUILD_CONFIGURATION="Release"
-readonly BASE_XCCONFIG_FILENAME="Base"
-readonly XCCONFIG_FILENAME="Release-Production"
-
+readonly SCHEME=${target_scheme}
 readonly DOCC_ARCHIVE_NAME="${SCHEME}.doccarchive"
 readonly DOCC_ARCHIVE_PATH="${BUILD_DIR}/${DOCC_ARCHIVE_NAME}"
 
-echo "YO PROJECT PATH IS ${PROJECT_PATH}"
-
 # Subdirectory of the devdocs S3 bucket where SDK static docs are stored
 # in a directory corresponding to the SDKs version. Ex: "${DOCS_ROOT_DIR}/1.0.0"
-readonly DOCS_ROOT_DIR="spaces-ios"
+readonly DOCS_ROOT_DIR=${devdocs_bucket}
 
 if ! command -v xcbeautify &> /dev/null
 then
@@ -35,19 +27,15 @@ rm -rf ${BUILD_DIR}
 
 echo "▸ Using Xcode Version: ${XCODE}"
 
-echo "▸ Building Documentation Catalog for ${SCHEME} located in ${PROJECT_PATH}"
+echo "▸ Building Documentation Catalog for ${SCHEME}"
 
 mkdir -p $DOCUMENTATION_DIR
 
-expanded_xcconfig_path="${XCCONFIG_DIR}/${XCCONFIG_FILENAME}.xcconfig"
-
-# -xcconfig $expanded_xcconfig_path \
-xcodebuild docbuild -project $PROJECT_PATH \
-                    -scheme $SCHEME \
-                    -configuration $BUILD_CONFIGURATION \
+xcodebuild docbuild -scheme $SCHEME \
                     -destination 'generic/platform=iOS' \
                     -sdk iphoneos \
-                    -derivedDataPath "${DOCUMENTATION_DIR}" | xcbeautify \
+                    -derivedDataPath "${DOCUMENTATION_DIR}" \
+                    | xcbeautify 
 
 cd ${BUILD_DIR}
 
@@ -58,22 +46,14 @@ docc_built_archive_path=$(find docs -type d -name "${DOCC_ARCHIVE_NAME}")
 
 if [ -z "${docc_built_archive_path}" ]
 then
-echo -e "\033[1;31m ERROR: Failed to locate Documentation Archive \033[0m"
+    echo -e "\033[1;31m ERROR: Failed to locate Documentation Archive \033[0m"
     exit 1
 else
     echo "▸ Located documentation archive at ${docc_built_archive_path}"
-    mv ${docc_built_archive_path} ${BUILD_DIR}
+    #mv ${docc_built_archive_path} ${BUILD_DIR}
+    cp -r ${docc_built_archive_path} ${BUILD_DIR}
     zip -qry "${DOCC_ARCHIVE_NAME}.zip" "${DOCC_ARCHIVE_NAME}"
 fi
-
-expanded_base_xcconfig_path="${XCCONFIG_DIR}/${BASE_XCCONFIG_FILENAME}.xcconfig"
-
-# Locates the SDK marketing version and extracts it to a variable
-#
-# Uses the substitute (s) command to lines containing 'MARKETING_VERSION = ' 
-# substring then captures everything on that line after the subscript (\(.*\)) 
-# then prints the first such match
-sdk_semantic_version="$(sed -n 's/MARKETING_VERSION = \(.*\)/\1/p' $expanded_base_xcconfig_path)"
 
 # When deploying a new version the contents of the latest subdirectory 
 # gets replaced with the generated documentation from the new version 
@@ -93,17 +73,23 @@ $(xcrun --find docc) process-archive transform-for-static-hosting "${source_arch
     --output-path "${output_path}" \
     --hosting-base-path "${hosting_base_path}"
 
-# Replace index.html with a redirect to documentation/muxspaces/
-sed -e "s/__VERSION__/${sdk_semantic_version}/" "${TOP_DIR}/scripts/index.html.template" > ${output_path}/index.html
+# Replace index.html with a redirect to documentation/your-lib-name/ for your version
+sed -e "s/__VERSION__/${sdk_semantic_version}/g" \
+    -e "s/__SLUG__/${DOCS_ROOT_DIR}/g" \
+    "scripts/docc-files/index.html.template" > ${output_path}/index.html
 
 mkdir -p $DOCS_ROOT_DIR
 
 mkdir -p $DOCS_ROOT_DIR/$latest_subdirectory_name
 
-# Replace index.html with a redirect to documentation/muxspaces/
-sed -e "s/__VERSION__/${latest_subdirectory_name}/" "${TOP_DIR}/scripts/index.html.template" > $DOCS_ROOT_DIR/${latest_subdirectory_name}/index.html
+# Replace index.html with a redirect to documentation/your-lib-name/ for 'latest'
+sed -e "s/__VERSION__/${latest_subdirectory_name}/" \
+    -e "s/__SLUG__/${DOCS_ROOT_DIR}/g" \
+    "scripts/docc-files/index.html.template" > $DOCS_ROOT_DIR/${latest_subdirectory_name}/index.html
 
+# For the record this overwrites our faked index.html but that helps it work
 cp -r ./$output_path/. ./$DOCS_ROOT_DIR/$latest_subdirectory_name
+
 
 mv $output_path $DOCS_ROOT_DIR
 
---
+++ scripts/post-process-docc-for-github-pages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+base_url="https://muxinc.github.io"
+repository_name="swift-upload-sdk"
+target_name="muxuploadsdk"
+output_path="docs/"
+
+sed -e "s/__BASE__/${base_url}/g" \
+    -e "s/__SLUG__/${repository_name}/g" \
+    -e "s/__TARGET__/${target_name}/g" \
+    "docc-files/index.html.template" > ${output_path}/index.html
+
+cat ${output_path}/index.html
