610c138f9e2c8f4071f7e9d4caf5b7c7909d68e9
AJ Lauer Barinov <abarinov@mux.com>
2023-08-02 13:13:25 -0700

    Logs

--- scripts/post-process-docc-for-github-pages.sh
+++ scripts/post-process-docc-for-github-pages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "▸ Adding redirect from the docc static archive root"
+
 host="muxinc.github.io"
 repository_name="swift-upload-sdk"
 target_name="muxuploadsdk"
@@ -9,5 +11,7 @@ sed -e "s/__HOST__/${host}/g" \
     -e "s/__SLUG__/${repository_name}/g" \
     -e "s/__TARGET__/${target_name}/g" \
     "scripts/docc-files/index.html.template" > ${output_path}/index.html
+
+echo "▸ Rewrote ${output_path}/index.html to:"
 
 cat ${output_path}/index.html
