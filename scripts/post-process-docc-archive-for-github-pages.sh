9e436d3222e52a3702d1e90b9933fda6fa15437b
AJ Lauer Barinov <abarinov@mux.com>
2023-08-02 13:32:02 -0700

    Make script reusable

--- scripts/post-process-docc-archive-for-github-pages.sh
+++ scripts/post-process-docc-archive-for-github-pages.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+if [ -z $1 ]
+then
+    echo -e "\033[1;31m ERROR: this script requires a repository name parameter. \033[0m"
+    exit $E_MISSING_POS_PARAM
+fi
+
+if [ -z $2 ]
+then
+    echo -e "\033[1;31m ERROR: this script requires a target name parameter. \033[0m"
+    exit $E_MISSING_POS_PARAM
+fi
+
 echo "▸ Adding redirect from the docc static archive root"
 
-host="muxinc.github.io"
-repository_name="swift-upload-sdk"
-target_name="muxuploadsdk"
 output_path="docs"
 
-sed -e "s/__HOST__/${host}/g" \
-    -e "s/__SLUG__/${repository_name}/g" \
-    -e "s/__TARGET__/${target_name}/g" \
+sed -e "s/__SLUG__/${1}/g" \
+    -e "s/__TARGET__/${2}/g" \
     "scripts/docc-files/index.html.template" > ${output_path}/index.html
 
 echo "▸ Rewrote ${output_path}/index.html to:"
