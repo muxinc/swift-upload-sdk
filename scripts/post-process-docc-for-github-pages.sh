cd59a7788fe4eb7966834cd6d6f9eb0fcbd7cdd9
AJ Lauer Barinov <abarinov@mux.com>
2023-08-02 12:51:56 -0700

    No escape

--- scripts/post-process-docc-for-github-pages.sh
+++ scripts/post-process-docc-for-github-pages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-base_url="https://muxinc.github.io"
+host="muxinc.github.io"
 repository_name="swift-upload-sdk"
 target_name="muxuploadsdk"
 output_path="docs/"
 
-sed -e "s/__BASE__/${base_url}/g" \
+sed -e "s/__HOST__/${host}/g" \
     -e "s/__SLUG__/${repository_name}/g" \
     -e "s/__TARGET__/${target_name}/g" \
     "scripts/docc-files/index.html.template" > ${output_path}/index.html
