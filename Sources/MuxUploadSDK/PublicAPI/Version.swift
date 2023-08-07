16a37a6a94428999ef98df01d220c5e74edcf533
AJ Lauer Barinov <abarinov@mux.com>
2023-08-07 11:26:12 -0700

    fix: adjust version

--- Sources/MuxUploadSDK/PublicAPI/Version.swift
+++ Sources/MuxUploadSDK/PublicAPI/Version.swift
@@ -11,9 +11,9 @@ public struct Version {
   /// Major version.
   public static let major = 0
   /// Minor version.
-  public static let minor = 5
+  public static let minor = 6
   /// Revision number.
-  public static let revision = 1
+  public static let revision = 0
 
   /// String form of the version number.
   public static let versionString = "\(major).\(minor).\(revision)"
