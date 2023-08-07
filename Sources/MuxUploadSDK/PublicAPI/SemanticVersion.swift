60d4d8f48d32c0f30cad25d15d0d6982416237c8
AJ Lauer Barinov <abarinov@mux.com>
2023-08-07 11:23:06 -0700

    Indicate Semantic Version explicitly in API
    Rename revision to patch as in SemVer 2.0.0
    
    Rename Version to SemanticVersion for explicitness

--- Sources/MuxUploadSDK/PublicAPI/SemanticVersion.swift
+++ Sources/MuxUploadSDK/PublicAPI/SemanticVersion.swift
@@ -1,5 +1,5 @@
 //
-//  Version.swift
+//  SemanticVersion.swift
 //  
 //
 //  Created by Emily Dixon on 2/21/23.
@@ -8,14 +8,14 @@
 import Foundation
 
 /// Version information about the SDK
-public struct Version {
+public struct SemanticVersion {
   /// Major version.
   public static let major = 0
   /// Minor version.
   public static let minor = 6
-  /// Revision number.
-  public static let revision = 0
+  /// Patch version.
+  public static let patch = 0
 
   /// String form of the version number.
-  public static let versionString = "\(major).\(minor).\(revision)"
+  public static let versionString = "\(major).\(minor).\(patch)"
 }
