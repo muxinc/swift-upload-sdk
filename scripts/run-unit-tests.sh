b98b811c6ab71d2174bafdb058016a94fa45edac
AJ Lauer Barinov <abarinov@mux.com>
2024-06-11 14:47:15 -0700

    use the right cli args

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -40,5 +40,5 @@ echo "▸ Test ${SCHEME}"
 xcodebuild clean test \
 	-scheme $SCHEME \
 	-destination 'platform=iOS Simulator,OS=17.5,name=iPhone 15' \
-	-sdk iphonesimulator17.4 \
+	-sdk iphonesimulator17.5 \
   | xcbeautify
